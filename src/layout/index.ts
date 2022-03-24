a90dd85bad449f961286e58980d9971cbf1293fa
TheTurkeyDev <turkey@theturkey.dev>
2022-03-23 21:02:31 -0400

    Added initial table logic

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,3 +1,4 @@
 export * from './card';
 export * from './navbar';
 export * from './space-between';
+export * from './table';
--- src/layout/table.tsx
+++ src/layout/table.tsx
@@ -1,5 +1,30 @@
-import styled from 'styled-components';
+import styled, { ThemeProps } from 'styled-components';
+import { BaseTheme } from '../theme/turkeydev-theme';
+import { Body1Css, Subtitle1Css } from '../typography';
 
 export const Table = styled.table`
+    background: ${({ theme }: ThemeProps<BaseTheme>) => theme.surface.color};
+    color: ${({ theme }: ThemeProps<BaseTheme>) => theme.surface.on};
+    width: 100%;
+    height: 100%;
 
+    & th {
+        border-top: 1px solid ${({ theme }: ThemeProps<BaseTheme>) => theme.surface.on};
+        border-bottom: 2px solid ${({ theme }: ThemeProps<BaseTheme>) => theme.surface.on};
+        padding: 8px;
+    }
+
+    & td {
+        border-top: 1px solid ${({ theme }: ThemeProps<BaseTheme>) => theme.surface.on};
+        padding: 8px;
+    }
+`;
+
+export const TH = styled.th`
+    ${Subtitle1Css}
+    font-weight: 600;
+`;
+
+export const TD = styled.td`
+    ${Body1Css}
 `;
