4852c82ef1a752f45dee50e3c36444e6350e9b5e
TheTurkeyDev <turkey@theturkey.dev>
2022-03-23 22:03:03 -0400

    Fixing LinkButton styling

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -1,20 +1,7 @@
 import { Link } from 'react-router-dom';
-import styled, { ThemeProps } from 'styled-components';
-import { BaseTheme } from '../theme/turkeydev-theme';
-import { ButtonTextCss } from '../typography';
+import styled from 'styled-components';
+import { Body1Css } from '../typography';
 
 export const LinkButton = styled(Link)`
-    ${ButtonTextCss}
-    padding: 8px 16px;
-    color: ${({ theme }: ThemeProps<BaseTheme>) => theme.primary.color};
-
-    &:hover {
-        text-decoration: none;
-        cursor: pointer;
-    }
-
-    &:disabled {
-        color:${({ theme }) => theme.outline};
-        cursor: not-allowed;
-    }
+   ${Body1Css}
 `;
