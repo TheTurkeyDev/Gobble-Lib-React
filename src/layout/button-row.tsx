86cfe0290f065dd3d2bc2dff1f806b0344515856
TheTurkeyDev <turkey@theturkey.dev>
2022-03-23 23:35:11 -0400

    Added button row

---
+++ src/layout/button-row.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ButtonRow = styled.div`
+    display: flex;
+    gap: 8px;
+    justify-content: flex-end;
+`;
--- src/layout/index.ts
+++ src/layout/index.ts
@@ -2,3 +2,4 @@ export * from './card';
 export * from './navbar';
 export * from './space-between';
 export * from './table';
+export * from './button-row';
