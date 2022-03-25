a80511598848c426da4ff8c678940856d174fce0
TheTurkeyDev <turkey@theturkey.dev>
2022-03-25 17:04:13 -0400

    Fixing Inputs Wrapper width and allowing it to be set to full width

--- src/form/inputs-wrapper.tsx
+++ src/form/inputs-wrapper.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
-export const InputsWrapper = styled.div`
+type InputsWrapperProps = {
+    readonly fullWidth?: boolean
+}
+
+export const InputsWrapper = styled.div<InputsWrapperProps>`
     display: grid;
     grid-template-columns: auto 1fr;
     gap: 16px;
     align-items: center;
+    width: ${({ fullWidth }) => fullWidth ? '100%' : 'fit-content'};
 `;
