fb44da19424cd1a1c2f9af809b1cc77b31903bfb
TheTurkeyDev <turkey@theturkey.dev>
2022-06-23 18:20:16 -0400

    Fixing input disabled css

--- src/form/input.tsx
+++ src/form/input.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from 'react';
 import styled, { ThemeProps } from 'styled-components';
 import { Opacity } from '../constants/opacity';
-import { BaseTheme } from '../theme';
+import { BaseTheme, GLThemeProps } from '../theme';
 import { Subtitle1Css } from '../typography/typography';
 import { Label } from './label';
 
@@ -25,20 +25,23 @@ const StyledInput = styled.input`
     }
 
     &:disabled {
-        background: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.colorDisabled};
         color: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.onDisabled};
         cursor: not-allowed;
     }
 
     &:read-only {
-        background: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.colorDisabled};
         color: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.onDisabled};
     }
 `;
 
-const InputContainer = styled.div`
-    background-color: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.color};
-    color:  ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.onVariant};
+type InputContainerProps = {
+    readonly disabled: boolean
+    readonly readOnly: boolean
+};
+
+const InputContainer = styled.div<InputContainerProps>`
+    background-color: ${({ theme, disabled, readOnly }: GLThemeProps<InputContainerProps>) => disabled || readOnly ? theme.inputs.colorDisabled : theme.inputs.color};
+    color:  ${({ theme, disabled, readOnly }: GLThemeProps<InputContainerProps>) => disabled || readOnly ? theme.inputs.onDisabled : theme.inputs.onVariant};
     padding: 0 12px 0 16px;
     border-radius: 4px;
     border: 1px solid ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.outlineLowered};
@@ -46,6 +49,7 @@ const InputContainer = styled.div`
     display: grid;
     grid-template-columns: auto 1fr auto;
     transition: background-color 0.2s;
+    cursor:  ${({ disabled, readOnly }: GLThemeProps<InputContainerProps>) => disabled || readOnly ? 'not-allowed' : ''};
 
     &:focus-within {
         color:  ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.on};
@@ -53,17 +57,6 @@ const InputContainer = styled.div`
 
     &:hover&:not(:focus-within) {
         opacity: ${Opacity.HOVER_MEDIUM};
-    }
-
-    &[disabled] {
-        background: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.colorDisabled};
-        color: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.onDisabled};
-        cursor: not-allowed;
-    }
-
-    &[read-only] {
-        background: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.colorDisabled};
-        color: ${({ theme }: ThemeProps<BaseTheme>) => theme.inputs.onDisabled};
     }
 `;
 
@@ -75,7 +68,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(({ id, label, ...p
     return (
         <>
             {label && <Label htmlFor={id}>{label}</Label>}
-            <InputContainer>
+            <InputContainer disabled={props.disabled ?? false} readOnly={props.readOnly ?? false}>
                 <Icon />
                 <StyledInput aria-label={`${id}-input`} id={id} ref={ref} {...props} />
                 <Icon />
