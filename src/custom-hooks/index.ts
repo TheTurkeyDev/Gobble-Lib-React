c971765c6aa7d5a1df40fb9880409723e6637bca
TheTurkeyDev <turkey@theturkey.dev>
2023-02-15 10:01:04 -0500

    Forgot to export fetch and query hooks

--- src/custom-hooks/index.ts
+++ src/custom-hooks/index.ts
@@ -2,3 +2,5 @@ export * from './use-click-outside';
 export * from './use-previous';
 export * from './use-url-params';
 export * from './use-interval';
+export * from './use-fetch';
+export * from './use-query';
